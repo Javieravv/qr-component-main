ebfa543bb15e1415b7c861dc7b48999d5623863a
Javier Vargas - Legion <javieravargasestudiante@gmail.com>
2023-03-18 18:29:53 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <section className="sectionMain">
+        <img src="/images/image-qr-code.png" alt="Código" />
+        <h3>Improve your front-end skills by building projects</h3>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </section>
+  )
+}
+
+export default App
